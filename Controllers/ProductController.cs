ae3679598d4ded66f34920dd7c71b724eedb4217
flaviofz <flaviofz@gmail.com>
2020-09-30 07:29:12 -0300

    GetById e GetByCategory, todos recebendo por id

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using _7194SHOP.Data;
 using _7194SHOP.Models;
@@ -21,6 +22,40 @@ namespace _7194SHOP.Controllers
                 .Include(x => x.Category)
                 .AsNoTracking()
                 .ToListAsync();
+
+            return products;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> Get
+        (
+            [FromServices] DataContext context,
+            int id
+        )
+        {
+            var product = await context.Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return product;
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory
+        (
+            [FromServices] DataContext context,
+            int id
+        )
+        {
+            var products = await context.Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .Where(x => x.CategoryId == id)
+                .ToListAsync();
+
             return products;
         }
 
